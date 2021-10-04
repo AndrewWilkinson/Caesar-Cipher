0c7a19cd8402a3edc0d8d3fdc446d983fb90f097
AndrewWilkinson <AndrewWilkinson@users.noreply.github.com>
2021-10-04 14:04:55 +0100

    simple cipher now passing all tests

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,15 +1,94 @@
-function caesarCipher(key, text)
-{
-    let newText = "";
+function caesarCipher(text, key) {
 
-    if (! text instanceof string)
-    {
+    if (text === "")
+    return "";
+
+    if (!text instanceof String) {
         return;
     }
 
-    newText = text; 
+    let newText = "";
+
+    let letterShift = key % 26;
+    let numberShift = key % 10;
+
+    for (var i = 0; i < text.length; i++) 
+    {
+        let characterCode = text.charCodeAt(i);
+
+        //lowercase range
+        if (characterCode > 96 && 123 > characterCode)
+        {
+            characterCode += letterShift;
+
+            if (characterCode > 122)
+            {
+                characterCode = 97 + (characterCode % 123);
+            }
+
+            else if (characterCode < 97)
+            {
+                characterCode = 122 - (96 - characterCode);
+            }
+        }
+
+        //uppercase range
+        else if (characterCode > 64 && 91 > characterCode)
+        {
+            characterCode += letterShift;
+
+            if (characterCode > 90)
+            {
+                characterCode = 65 + (characterCode % 91);
+            }
+
+            else if (characterCode < 65)
+            {
+                characterCode = 90 - (64 - characterCode);
+            }
+        }
+
+        //numbers range
+        else if (characterCode > 47 && 58 > characterCode)
+        {
+            characterCode += numberShift;
+
+            if (characterCode > 57)
+            {
+                characterCode = 48 + (characterCode % 58);
+            }
+
+            else if (characterCode < 48)
+            {
+                characterCode = 57 - (47 - characterCode);
+            }
+        }
+
+        newText += String.fromCharCode(characterCode);
+    }
+
+    //newText = text; //TEMP
 
     return newText;
 
 }
 
+function rollingCaesarCipher(text, minRoll, maxRoll) {
+    if (!text instanceof string) {
+        return;
+    }
+    let newText = "";
+
+    for (var i = 0, roll = 0; i < text.length; i++, roll++) 
+    {
+        newText += caesarCipher(text[i], minRoll+roll);
+        if (roll == maxRoll) {roll = 0};
+    }
+
+}
+
+//TODO something clever about numbers
+
+export {
+    caesarCipher // , rollingCaesarCipher
+}
---
+++ caesarCipher.spec.js
@@ -0,0 +1,103 @@
+import { caesarCipher } from "./caesarCipher.js";
+
+describe("Caesar Cipher", () => {
+
+    test("given '' and a key of 1: results in '' ", () => {
+        const result = caesarCipher("", 1);
+        expect(result).toBe("")
+    })
+
+    test("given 'abc' and a key of 0: results in 'abc' ", () => {
+        const result = caesarCipher("abc", 0);
+        expect(result).toBe("abc")
+    })
+
+    test("given 'a' and a key of 1: results in 'a' ", () => {
+        const result = caesarCipher("a", 1);
+        expect(result).toBe("b")
+    })
+
+    test("given the alphabet and a key of 1: results in alphabet starting at 'b' ending at 'a' ", () => {
+        const result = caesarCipher("abcdefghijklmnopqrstuvwxyz", 1);
+        expect(result).toBe("bcdefghijklmnopqrstuvwxyza")
+    })
+
+    test("given 'bcd' and a key of 1: results in 'cde' ", () => {
+        const result = caesarCipher("bcd", 1);
+        expect(result).toBe("cde")
+    })
+
+    test("given 'ABC' and a key of 1: results in 'BCD' ", () => {
+        const result = caesarCipher("ABC", 1);
+        expect(result).toBe("BCD")
+    })
+
+    test("given 'aBc' and a key of 1: results in 'bCd' ", () => {
+        const result = caesarCipher("aBc", 1);
+        expect(result).toBe("bCd")
+    })
+
+    //larger keys
+
+    test("given 'abc' and a medium-sized key of 10: results in 'qrs' ", () => {
+        const result = caesarCipher("abc", 10);
+        expect(result).toBe("klm")
+    })
+
+    test("given 'abc' and a very large key of 26: results in 'abc' ", () => {
+        const result = caesarCipher("abc", 26);
+        expect(result).toBe("abc")
+    })
+
+    test("given 'abc' and a very large key of 27: results in 'bcd' ", () => {
+        const result = caesarCipher("abc", 27);
+        expect(result).toBe("bcd")
+    })
+
+    //negative keys
+
+    test("given 'abc' and a negative key of -1: results in 'zab' ", () => {
+        const result = caesarCipher("abc", -1);
+        expect(result).toBe("zab")
+    })
+
+    test("given 'abc' and a very large negative key of -26: results in 'abc' ", () => {
+        const result = caesarCipher("abc", -26);
+        expect(result).toBe("abc")
+    })
+
+    test("given 'abc' and a very large negative key of -27: results in 'zab' ", () => {
+        const result = caesarCipher("abc", -27);
+        expect(result).toBe("zab")
+    })
+
+    //numbers
+
+    test("given '123' and a key of 1: results in '234' ", () => {
+        const result = caesarCipher("123", 1);
+        expect(result).toBe("234")
+    })
+
+    test("given '890' and a key of 1: results in '901' ", () => {
+        const result = caesarCipher("890", 1);
+        expect(result).toBe("901")
+    })
+
+    test("given '123' and a key of 10: results in '123' ", () => {
+        const result = caesarCipher("123", 0);
+        expect(result).toBe("123")
+    })
+
+    test("given '123' and a negative key of -2: results in '901' ", () => {
+        const result = caesarCipher("123", -2);
+        expect(result).toBe("901")
+    })
+
+    //mixed inputs
+
+    test("given 'a2C' and a key of 1: results in 'b3D' ", () => {
+        const result = caesarCipher("a2C", 1);
+        expect(result).toBe("b3D")
+    })
+
+})
